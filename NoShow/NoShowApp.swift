14c41b14c3a9487f998bb4c0dcb4a04761c2a5c5
nataliaivan0v <nataliaivanov2004@gmail.com>
2026-03-20 17:17:43 -0400

    Initial Commit

---
+++ NoShow/NoShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoShowApp.swift
+//  NoShow
+//
+//  Created by Natalia Ivanov on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct NoShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
